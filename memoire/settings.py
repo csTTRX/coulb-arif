f88dae576bb3ff1b813694a704351c8605141668
csTTRX <cs.ttrx@gmail.com>
2022-06-01 02:30:17 +0100

    first

--- memoire/settings.py
+++ memoire/settings.py
@@ -129,7 +129,7 @@ EMAIL_PORT = 587
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
